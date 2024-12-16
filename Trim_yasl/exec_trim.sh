cb7b4cbedf3da7abd4c383d5814787c428dde7a5
Ondřej Valach <65716432+oValach@users.noreply.github.com>
2024-12-16 20:16:38 +0100

    Update exec_trim.sh

--- Trim_yasl/exec_trim.sh
+++ Trim_yasl/exec_trim.sh
@@ -3,10 +3,10 @@
 #PBS -l walltime=10:00:00
 #PBS -N trim2
 
-LOG_DIR="/auto/plzen1/home/valacho/SignLLM/trim/files_timestamps/files_timestamps1.csv"
-IN_DIR="/auto/plzen1/home/mhruz/JSALT2024/YouTubeASL/videos/videos/"
-OUT_DIR="auto/plzen1/home/mhruz/JSALT2024/YouTubeASL/clips/"
-
+LOG_DIR="/files_timestamps/files_timestamps1.csv"
+IN_DIR="YouTubeASL/videos/videos/"
+OUT_DIR="YouTubeASL/clips/"
+FFMPEG="/ffmpeg/"
 
 module load mambaforge
 conda activate Trim
@@ -15,3 +15,4 @@ python script_trim.py \
 	--inputdir ${IN_DIR} \
 	--csv_dir ${LOG_DIR} \
 	--output ${OUT_DIR} \
+ 	--ffmpeg ${FFMPEG}
