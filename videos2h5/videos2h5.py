4828e160f0cee51ebde521178df3772fb31f7f60
marekhruz <mhruz@ntis.zcu.cz>
2025-04-21 18:04:06 +0200

    Basic functionality, saving and loading raw videos to h5

---
+++ videos2h5/videos2h5.py
@@ -0,0 +1,48 @@
+import argparse
+import h5py
+import os
+
+
+def save_raw_videos_to_h5(video_paths, h5_path):
+    with h5py.File(h5_path, 'w') as f:
+        for path in video_paths:
+            video_name = os.path.splitext(os.path.basename(path))[0]
+            with open(path, 'rb') as file:
+                byte_data = file.read()
+                byte_array = bytearray(byte_data)
+                f.create_dataset(video_name, data=byte_array, dtype='uint8')
+
+
+def load_raw_video_from_h5(h5_path, video_name, output_path):
+    with h5py.File(h5_path, 'r') as f:
+        byte_array = f[video_name][:]
+        with open(output_path, 'wb') as out_file:
+            out_file.write(byte_array.tobytes())
+
+
+def find_video_files(directory, extensions=None):
+    if extensions is None:
+        extensions = ['.mp4', '.avi', '.mov', '.mkv']
+    video_files = []
+    for fname in os.listdir(directory):
+        if any(fname.lower().endswith(ext) for ext in extensions):
+            video_files.append(os.path.join(directory, fname))
+    return video_files
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Store video files as raw bytes in an HDF5 file.')
+    parser.add_argument('input_dir', type=str, help='Directory containing video files.')
+    parser.add_argument('output_h5', type=str, help='Output HDF5 file path.')
+    parser.add_argument('--ext', nargs='*', default=['.mp4', '.avi', '.mov', '.mkv'],
+                        help='List of video file extensions to include (default: .mp4 .avi .mov .mkv)')
+    args = parser.parse_args()
+
+    video_files = find_video_files(args.input_dir, args.ext)
+    if not video_files:
+        print("No video files found in the specified directory with the given extensions.")
+        return
+
+    print(f"Found {len(video_files)} video files. Saving to {args.output_h5}")
+    save_raw_videos_to_h5(video_files, args.output_h5)
+    print("Done.")
